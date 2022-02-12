ed94b20dae170a599b3bd9d291363dea0e35569e
ozgencdev <ozgencdev@gmail.com>
2022-02-12 19:36:57 +0300

    test - 3

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -4,12 +4,15 @@ const {
   getUsers,
   addExercises,
   getUserLogs,
+  getExercises,
 } = require("../handler/api.handler");
 
 route.get("/api/users", getUsers); //tüm kullanıcıları alacaksın
 route.post("/api/users", createUser);
 
 route.get("/api/users/:_id/logs", getUserLogs);
+
+route.get("/api/users/:_id/exercises", getExercises);
 route.post("/api/users/:_id/exercises", addExercises);
 
 exports.route = route;
